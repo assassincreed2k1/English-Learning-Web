9b4ea63b2422c56b02f3753ff4f557c3b2354c09
Nguyen Trung Nghia <trungnghia180205@gmail.com>
2025-04-16 02:58:00 +0700

    nghia

--- src/main/java/com/englishlearning/model/system/VocabularyExam.java
+++ src/main/java/com/englishlearning/model/system/VocabularyExam.java
@@ -3,17 +3,13 @@ package com.englishlearning.model.system;
 import com.englishlearning.model.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "vocabularyExams")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class VocabularyExam extends BaseEntity {
 
 }
