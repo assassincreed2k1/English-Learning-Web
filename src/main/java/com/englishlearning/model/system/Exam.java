806ff942ede9e0e4eed8bfd00e195ab27822b11b
Nguyen Thanh Bang <nguyenthanhbang2206@gmail.com>
2025-04-17 22:23:17 +0700

    add security

--- src/main/java/com/englishlearning/model/system/Exam.java
+++ src/main/java/com/englishlearning/model/system/Exam.java
@@ -1,14 +1,21 @@
 package com.englishlearning.model.system;
 
-import com.englishlearning.model.BaseEntity;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.englishlearning.model.BaseEntity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 @Entity
 @Table(name = "exams")
 @Getter
@@ -19,21 +26,23 @@ public class Exam extends BaseEntity {
     private String title;
     private String image;
     private Integer duration;
-    private Integer totalQuestions;
-
-    private String audioLink;
-    @Column(columnDefinition = "TEXT")
-    private String script;
+    private Integer totalAssignment;
 
     @Enumerated(EnumType.STRING)
     private ExamType examType;
-    public enum ExamType {
-        GRAMMAR, READING, LISTENING, VOCABULARY
+
+    @OneToMany(mappedBy = "exam", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<ExamAssignment> examAssignments = new ArrayList<>();
+
+    public void addQAssignmentFromEntity(Assignment assignment) {
+        ExamAssignment ea = new ExamAssignment();
+        ea.setExam(this);
+        ea.setAssignment(assignment);
+        this.examAssignments.add(ea);
     }
 
-    @OneToMany(mappedBy = "exam", cascade = CascadeType.ALL)
-    private List<Question> questions;
+    public enum ExamType {
+        VOCABULARY, GRAMMAR, LISTENING, READING,
+    }
 
-    @OneToMany(mappedBy = "exam")
-    private List<ExamResult> examResults;
 }
