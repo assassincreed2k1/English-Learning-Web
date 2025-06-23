6a2fb8b79f44159eadbc04815aa3f0392b956107
Nguyen Trung Nghia <trungnghia180205@gmail.com>
2025-06-23 22:54:11 +0700

    Assignment Service: find by content or description

--- backend/src/main/java/com/englishlearning/repository/AssignmentRepository.java
+++ backend/src/main/java/com/englishlearning/repository/AssignmentRepository.java
@@ -1,7 +1,11 @@
 package com.englishlearning.repository;
 
+import java.util.List;
+
 import com.englishlearning.model.system.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+    List<Assignment> findByDescriptionContainingIgnoreCaseOrTypeContainingIgnoreCase(String descriptionKeyword,
+            String typeKeyword);
 }
